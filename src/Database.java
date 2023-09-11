c21800dc940c422c59047ae7bed6e5b9c28fbb9d
Gerberg <masi0001@stud.kea.dk>
2023-09-11 11:52:31 +0200

    Main

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,14 @@
 public class Database {
+
+    private Superhero[] helteListe;
+    int count = 0;
+
+    public Database() {
+        this.helteListe = new Superhero[5];
+    }
+
+    public void tilføjSuperhelt(String name, String realName, String superPower, int yearCreated, String isHuman, String strength) {
+        helteListe[count++] = new Superhero(name, realName, superPower, yearCreated, isHuman, strength);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Opret en superhelt");
+        System.out.println("Hvad er navnet på din superhelt?");
+        String Navn = keyboard.next();
+        System.out.println("Hvad er superheltens virkelige navn?");
+        String virkeligeNavn = keyboard.next();
+        System.out.println("Hvad er superheltens superkraft?");
+        String superKraft = keyboard.next();
+        System.out.println("Hvilket år udkom din superhelt?");
+        String årSkabt = keyboard.next();
+        System.out.println("Er din superhelt et menneske?");
+        String erMenneske = keyboard.next();
+
 
     }
 }
